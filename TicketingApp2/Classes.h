b9a0d6aeff3af7d1c843dc4a031d74282a6f32eb
GabrielLeanca <gabriel.leanca@gmail.com>
2023-12-28 13:53:00 +0200

    added enum and tested it

--- TicketingApp2/Classes.h
+++ TicketingApp2/Classes.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <random>
+using namespace std;
+
+enum ticketType {
+	VIP, CATEGORY1, CATEGORY2, CATEGORY3, OTHER
+};
+
+ostream& operator<<(ostream& console, ticketType type) {
+	switch (type)
+	{
+	case VIP:
+		console << "VIP";
+		break;
+	case CATEGORY1:
+		console << "CATEGORY1";
+		break;
+	case CATEGORY2:
+		console << "CATEGORY2";
+		break;
+	case CATEGORY3:
+		console << "CATEGORY3";
+		break;
+	default:
+		console << "OTHER";
+	}
+	return console;
+}
+
+class Event;
+class Ticket;
+
---
+++ TicketingApp2/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Classes.h"
+using namespace std;
+
+int main() {
+
+	//tests
+	ticketType type = CATEGORY1;
+	cout << type;
+	//end of tests
+	//
+	//
+}
